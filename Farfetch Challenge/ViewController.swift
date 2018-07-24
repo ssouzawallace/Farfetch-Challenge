ca618560db3bbd619a9cd9822d141cb67eb966d5
Wallace Souza <ssouza.wallace@gmail.com>
2018-07-24 17:48:56 -0300

    Initial Commit

---
+++ Farfetch Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Farfetch Challenge
+//
+//  Created by Wallace Souza Silva on 24/07/18.
+//  Copyright © 2018 WallaCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
